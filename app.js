56fc0b6e315135c72f57f4e20f39d6a096d3370b
HARIHARA PRASATH <hari.hari840@gmail.com>
2021-01-07 17:40:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// server.js
 const express = require('express')
 const app = express()
-const port = 8080
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => {
+    res.send('Hello world from a Node.js app!')
+})
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+})
